db6afc1c70d86a775e5073886be4d291c7c65368
20juanpls <shortwavepeak11@gmail.com>
2016-11-16 12:00:01 -0600

    camera is now fixed to revolve around player and you can edit the position of the camera

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -3,25 +3,44 @@ using System.Collections;
 
 public class CameraScript : MonoBehaviour {
 	Transform player;
-	private Vector3  offset;
-	private Vector3 relativePos;
-	private Quaternion rotset;
-	public float SetCamRotSpeed = 100.0f;
+
+	Vector3 thisPos;
+
+	private Vector3 offset;
+
+	public float DistY = 8.0f;
+	public float DistX = 7.0f;
+
+	//private Vector3 relativePos;
+	//private Quaternion rotset;
+	public float SetCamRotSpeed = 1.0f;
 	private float CamRotSpeed = 0.0f; 
+	//private float yOrigOffset;
 	
 	void Start ()
 	{
 		player = GameObject.FindGameObjectWithTag ("PlayerMesh").GetComponent<Transform>();
-		offset = this.transform.position - player.transform.position;
+		thisPos = this.transform.position;
+		offset = new Vector3(player.position.x, player.position.y + DistY, player.position.z + DistX);
 	}
 	
 	void LateUpdate ()
 	{
-		//Camera following
-		//Debug.Log (offsetY);
-		//this.transform.position = player.transform.position + offset;
+		CamRotation ();
+		offset = Quaternion.AngleAxis (CamRotSpeed, Vector3.up) * offset;
+		transform.position = player.position + offset; 
+		transform.LookAt(player.position);
 
+
+
+		//Camera following
+		//this.transform.position = playerPos-thisPos;
+		//this.transform.Translate (offset);
 		//CameraOrbits --V
+
+
+	}
+	void CamRotation(){
 		if (Input.GetKey(KeyCode.I)) {
 			CamRotSpeed = SetCamRotSpeed;
 		}
@@ -30,13 +49,5 @@ public class CameraScript : MonoBehaviour {
 		} else {
 			CamRotSpeed = 0.0f;
 		}
-		//Vector3 HorizVect = new Vector3 (0.0f, HorizonCount, 0.0f);
-		this.transform.RotateAround(player.position, Vector3.up, CamRotSpeed * Time.deltaTime);
-		//this.transform.Translate(Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")));
-		//to look at player--Vs
-		relativePos = player.position - this.transform.position;
-		rotset = Quaternion.LookRotation (relativePos);
-		this.transform.rotation = rotset;
-
 	}
 }
--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -8,10 +8,12 @@ public class PlayerControls : MonoBehaviour {
 	private float angle_1;
 	public float jumpSpeed = -10.0f;
 	private Vector3 moveDirection = Vector3.zero;
-	private Vector3 moveAngle = Vector3.zero;
+	private Vector3 rotatedDirection = Vector3.zero;
+	private Quaternion _lookRotation;
 	Quaternion surfaceAngle;
 	public float moveSpeed = 0.5f;
 	private bool canitjump;
+	public float rotationSpeed = 1.0f;
 	// Use this for initialization
 	void Start () {
 		Prb = this.GetComponent<Rigidbody> ();
@@ -23,16 +25,22 @@ public class PlayerControls : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		float HorizMov = Input.GetAxis ("Horizontal");
+		float VertMov = Input.GetAxis ("Vertical");
+		moveDirection = new Vector3(HorizMov, 0, VertMov);
+		
 		ControlOrientation ();
+
+		//PlayerMeshOrientation (); <Dont use this just yet...>
+
 		if (Input.GetKeyDown ("space") && canitjump == true) {
 			Debug.Log ("is this touched");
 			JumpingC.setInitialSpeed(jumpSpeed);
 		}
 		canitjump = JumpingC.IsItGrounded();
+	
 	}
 	void ControlOrientation(){
-		float HorizMov = Input.GetAxis ("Horizontal");
-		float VertMov = Input.GetAxis ("Vertical");
 		surfaceAngle = JumpingC.currentSurfaceAngle();
 		//finds angle of camera relative to world & angle of surface
 		float cameraRot = Camera_Rot.rotation.eulerAngles.y;
@@ -41,15 +49,18 @@ public class PlayerControls : MonoBehaviour {
 		float EulerZ = -surfaceAngle.eulerAngles.z;
 		//Debug.Log (EulerX + "," + EulerZ);
 		//converts floats to horizontal and vertical value depending on camera orientation
-		moveDirection = new Vector3(HorizMov, 0, VertMov);
 		Quaternion qx = Quaternion.AngleAxis(EulerX, Vector3.right);
 		Quaternion qz = Quaternion.AngleAxis(EulerZ, Vector3.forward); 
 		Quaternion qy = Quaternion.AngleAxis(cameraRot, Vector3.up);
 		Quaternion q = qx * qz * qy;
 
-		Vector3 rotatedDirection = q * moveDirection;
+		rotatedDirection = q * moveDirection;
 		//Debug.DrawLine (Vector3.zero, rotatedDirection, Color.green);
 		//applies the direction to GamePbject Player rigidbody
 		Prb.transform.Translate (rotatedDirection*moveSpeed);
+	}
+	void PlayerMeshOrientation(){
+		_lookRotation =  new Quaternion(0.0f,Quaternion.LookRotation(rotatedDirection).y,0.0f,0.0f);
+		this.transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * rotationSpeed);
 	}
 }
