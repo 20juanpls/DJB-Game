6681cc591401922c6783db6b8482a64c0b11d253
Noah <Nore5515@gmail.com>
2016-11-23 17:44:17 -0600

    Test Map Design, Buttons

--- Assets/Scripts/PlayerScore.cs
+++ Assets/Scripts/PlayerScore.cs
@@ -6,19 +6,27 @@ public class PlayerScore : MonoBehaviour {
 	
 	public int score;
 	Text scoreText;
+	GameObject[] coins;
 
 	// Use this for initialization
 	void Start () {
 		score = 0;
 		scoreText = GameObject.Find ("ScoreText").GetComponent<Text> ();
 		scoreText.text = "Score: 0";
-		Debug.Log (scoreText.ToString ());
+		coins = GameObject.FindGameObjectsWithTag("Coin");
 	}
 	
 	// ScoreUpdate is called whenever a coin is collected
 	void ScoreUpdate () {
 		score++;
 		scoreText.text = "Score: " + score;
+
+		//VERY TEMP END SCREEN
+		if (score == coins.Length) {
+			scoreText.text = "YOU WIN!";
+		}
+
+
 	}
 
 	void OnTriggerEnter(Collider other){
