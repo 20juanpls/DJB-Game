fa9af0022ab12c74fc4435d5b4d2b0b8a161be53
Noah <Nore5515@gmail.com>
2016-11-23 17:23:31 -0600

    Buttons work and can by synced to other things (such as more buttons)

---
+++ Assets/ButtonActivator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonActivator : MonoBehaviour {
+
+	public GameObject activated;
+	public bool oneUse;
+	bool ded;
+
+	void Start(){
+		activated.SetActive (!activated.activeSelf);
+		ded = false;
+	}
+
+	public void ButtonActivated(){
+		if (ded == false) {
+			Debug.Log ("fixin");
+			activated.SetActive (!activated.activeSelf);
+			if (oneUse) {
+				ded = true;
+				this.transform.parent.transform.GetChild (1).gameObject.SetActive (false);
+			}
+		}
+	}
+
+}
