53b622d8a6d273c69118b26a36796efd2960c31a
Noah Reed <Nore5515@Gmail.com>
2016-11-28 17:46:58 -0600

    Merged with animation and worked on checkpoints+gameover.

---
+++ Assets/Checkpoint.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkpoint : MonoBehaviour {
+
+	public bool activated;
+	GameObject flag;
+
+	// Use this for initialization
+	void Start () {
+		activated = false;
+		flag = this.gameObject.transform.FindChild("Flag").gameObject;
+		flag.SetActive (false);
+	}
+
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.tag == "PlayerMesh") {
+			flag.SetActive (true);
+			GameObject.Find ("Player").GetComponent<PlayerLavaDeath> ().checkpointReached (this.gameObject);
+		}
+	}
+
+}
---
+++ Assets/Scripts/PlayerLavaDeath.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerLavaDeath : MonoBehaviour {
+
+	public GameObject loseScreen;
+	GameObject player;
+	Transform respawn;
+
+	// Use this for initialization
+	void Start () {
+		loseScreen = GameObject.Find ("LoseScreenCanvas");
+		loseScreen.SetActive (false);
+		player = GameObject.Find ("Player_folder");
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Kill") {
+			loseScreen.gameObject.SetActive (true);
+		}
+	}
+
+	public void checkpointReached(GameObject checkpointReached){
+		respawn = checkpointReached.transform;
+		Debug.Log ("Respawn point is now assigned to " + respawn.ToString ());
+	}
+
+	public void Restart(){
+		Instantiate (player, respawn);
+	}
+}
