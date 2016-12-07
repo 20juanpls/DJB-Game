b98ab3c5c395f16b90d2904e91f14d6144f868ae
Noah Reed <Nore5515@Gmail.com>
2016-12-07 16:35:44 -0600

    1/3 npc atk force, npcJumpCollider added

---
+++ Assets/Brutus State Machine/BrutusMechanimInputs.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrutusMechanimInputs : MonoBehaviour 
+{
+    public Animator thisAnimator;
+
+    void Start () 
+    {
+        thisAnimator = GetComponent<Animator>();
+    }
+
+    void Update () 
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            thisAnimator.SetTrigger("Runs");
+        }
+
+        if(Input.GetButtonDown("Fire2"))
+        {
+            thisAnimator.SetTrigger("Stops");
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            thisAnimator.SetBool("Jumps", true);
+        }
+        else
+        {
+            thisAnimator.SetBool("Jumps", false);
+        }
+    }
+}
