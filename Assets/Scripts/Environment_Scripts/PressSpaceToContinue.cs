138d2e6f62bd7e5e09534cd063bdcfd8ec3f210f
Noah Reed <Nore5515@Gmail.com>
2017-01-05 16:59:28 -0600

    Added intro screen, space to continue

---
+++ Assets/Scripts/Environment_Scripts/PressSpaceToContinue.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PressSpaceToContinue : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			SceneManager.LoadScene ("Bugfixer_0_0_1", LoadSceneMode.Additive);
+			Destroy (this.gameObject);
+		}
+			
+	}
+}
