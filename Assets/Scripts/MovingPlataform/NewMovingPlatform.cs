1c119e71939bb5a4c9d3b9d171e09d57b3aeeb10
Reed.Noah.s582060 <s582060@rrhs-943-18.rrisd.local>
2017-04-07 13:26:17 -0500

    noah update

---
+++ Assets/Scripts/MovingPlataform/NewMovingPlatform.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewMovingPlatform : MonoBehaviour {
+
+	//I want to be able to have this specific game object move from a list of given checkpoints
+	//I want to be able to set the time it travels between these points
+
+	public float time;
+	public List<GameObject> checkpoints;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (checkpoints.Capacity);
+		Debug.Log ("Start: " + checkpoints [0]);
+		Debug.Log ("Stop: " + checkpoints [1]);
+		this.transform.position = Vector3.Lerp (checkpoints [0].transform.position, checkpoints [1].transform.position, time);
+	}
+}
