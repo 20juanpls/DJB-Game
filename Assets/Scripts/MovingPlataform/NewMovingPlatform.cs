3c2d20a5658d286910a6ba9fd8110e072f0effed
Reed.Noah.s582060 <s582060@rrhs-943-18.rrisd.local>
2017-04-11 14:27:42 -0500

    Moving Platform 2.0 Added

--- Assets/Scripts/MovingPlataform/NewMovingPlatform.cs
+++ Assets/Scripts/MovingPlataform/NewMovingPlatform.cs
@@ -7,19 +7,56 @@ public class NewMovingPlatform : MonoBehaviour {
 	//I want to be able to have this specific game object move from a list of given checkpoints
 	//I want to be able to set the time it travels between these points
 
-	public float time;
+	public float time, buffer, deadzone;
 	public List<GameObject> checkpoints;
+	bool attatched;
+	Collision other;
+	int CDI, MPI;
 
 	// Use this for initialization
 	void Start () {
-		
+		MPI = 0;
+		CDI = 1;
+		this.transform.position = checkpoints [MPI].transform.position;
+		Debug.Log (checkpoints [CDI].transform.position + " - " + checkpoints [MPI].transform.position);
+		Vector3 velo = new Vector3 (checkpoints [CDI].transform.position.x - checkpoints [MPI].transform.position.x, checkpoints [CDI].transform.position.y - checkpoints [MPI].transform.position.y, checkpoints [CDI].transform.position.z - checkpoints [MPI].transform.position.z);
+		this.GetComponent<Rigidbody> ().velocity = (velo * buffer);
+		Debug.Log (this.GetComponent<Rigidbody> ().velocity);
+		attatched = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log (checkpoints.Capacity);
-		Debug.Log ("Start: " + checkpoints [0]);
-		Debug.Log ("Stop: " + checkpoints [1]);
-		this.transform.position = Vector3.Lerp (checkpoints [0].transform.position, checkpoints [1].transform.position, time);
+		if (attatched) {
+			Debug.Log ("Ow");
+			other.gameObject.GetComponent<Rigidbody> ().velocity += this.gameObject.GetComponent<Rigidbody> ().velocity;
+		}
+		Debug.Log (Vector3.Distance (this.transform.position, checkpoints [CDI].transform.position));
+		if (Vector3.Distance (this.transform.position, checkpoints [CDI].transform.position) <= deadzone) {
+			Debug.Log ("LOOP");
+			CDI++;
+			MPI++;
+			if (CDI >= checkpoints.Capacity) {
+				CDI = 0;
+			}
+			if (MPI >= checkpoints.Capacity) {
+				MPI = 0;
+			}
+			Vector3 velo = new Vector3 (checkpoints [CDI].transform.position.x - checkpoints [MPI].transform.position.x, checkpoints [CDI].transform.position.y - checkpoints [MPI].transform.position.y, checkpoints [CDI].transform.position.z - checkpoints [MPI].transform.position.z);
+			this.GetComponent<Rigidbody> ().velocity = (velo * buffer);
+		}
+
+
+	}
+
+
+	void OnCollisionEnter (Collision _other){
+		Debug.Log ("Hello, " + _other.ToString ());
+		attatched = true;
+		other = _other;
+	}
+	void OnCollisionExit(Collision _other){
+		Debug.Log ("Goodbye, " + _other.ToString ());
+		attatched = false;
 	}
 }
